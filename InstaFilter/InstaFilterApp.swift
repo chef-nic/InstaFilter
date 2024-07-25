d8a1d611e1c341d4d5f97d83556799bd873abf7d
chef-nic <nicholasajohnson@me.com>
2024-07-25 15:54:20 -0700

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Nicholas Johnson on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
